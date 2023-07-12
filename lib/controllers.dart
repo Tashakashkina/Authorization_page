f08566beb7a997878e2684b4ed35ee448253b7a2
Шпилевская Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-07-12 22:32:26 +0400

    undo changes

--- lib/controllers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-
-class _LoginState extends State<Login> {
-  TextEditingController controllerEmail = TextEditingController();
-  TextEditingController controllerUserName = TextEditingController();
-  TextEditingController controllerPassword = TextEditingController();
-  dynamic localStoragePass = SharedPreferences.getInstance();
-  dynamic localStorageName = SharedPreferences.getInstance();

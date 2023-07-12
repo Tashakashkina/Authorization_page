bf5773d58d48272042696b5fc0a567dbd7e51527
Шпилевская Лилия <84247746+Tashakashkina@users.noreply.github.com>
2023-07-12 22:30:48 +0400

    added texteditingcontrollers

--- lib/controllers.dart
+++ lib/controllers.dart
@@ -1,1 +1,7 @@
 
+class _LoginState extends State<Login> {
+  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerUserName = TextEditingController();
+  TextEditingController controllerPassword = TextEditingController();
+  dynamic localStoragePass = SharedPreferences.getInstance();
+  dynamic localStorageName = SharedPreferences.getInstance();
